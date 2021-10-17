f9e9e3b832139897def66a451086476ddc68e525
Seb <sebcyde@hotmail.com>
2021-10-17 08:41:15 +0100

    added in dynamic create elements script

--- app.js
+++ app.js
@@ -1,3 +1,74 @@
 // retrieve data then add entry numbers to array then create elements based on array order, can use array to sort from high to low and from low to high
 
-axios.get();
+axios
+	.get('https://pokeapi.co/api/v2/pokemon?limit=1118')
+	.then((results) => {
+		console.log(results.data.results);
+		let RawResults = results.data.results;
+		const LoadingSignal = document.querySelector('#LoadingSignal');
+		LoadingSignal.classList.add('Hidden');
+
+		RawResults.map((Poke) => {
+			const x = Poke.name;
+			const PokeCardContainer = document.createElement('div');
+
+			PokeCardContainer.classList.add('PokeCardContainer');
+			axios.get(`https://pokeapi.co/api/v2/pokemon/${x}`).then((results) => {
+				console.log(results);
+				const PokeCard = document.createElement('div');
+				PokeCard.classList.add('PokeCard');
+				PokeCardContainer.appendChild(PokeCard);
+				// const img =
+				// PokeCardContainer.appendChild(img);
+				const PokemonSpan = document.createElement('span');
+				PokemonSpan.classList.add('Pokemon');
+				PokeCardContainer.appendChild(PokemonSpan);
+				const PokemonName = document.createElement('h3');
+				PokemonName.classList.add('PokemonName');
+				PokemonName.innerText = Poke.name;
+				PokemonSpan.appendChild(PokemonName);
+				const PokemonTypes = document.createElement('div');
+				PokemonTypes.classList.add('PokemonTypes');
+				PokemonSpan.appendChild(PokemonTypes);
+				const PokemonEntryNumber = document.createElement('h4');
+				PokemonEntryNumber.classList.add('EntryNumber');
+				PokemonTypes.appendChild(PokemonEntryNumber);
+				const PokemonType1 = document.createElement('h4');
+				PokemonType1.classList.add('Type1');
+				PokemonTypes.appendChild(PokemonType1);
+				PokeCard.appendChild(PokemonSpan);
+				// if () {
+				//     const PokemonType2 = document.createElement('h4').classList.add('Type2');
+				//     PokemonTypes.appendChild(PokemonType2);
+				// }
+			});
+			document.querySelector('.LeftSection').appendChild(PokeCardContainer);
+		});
+	})
+	.catch((error) => {
+		console.log(error);
+	});
+
+/* <div class="PokeCardContainer">
+    <div class="PokeCard">
+        <img src="https://purepng.com/public/uploads/large/purepng.com-pokemonpokemonpocket-monsterspokemon-franchisefictional-speciesone-pokemonmany-pokemonone-pikachu-1701527784845bdjl3.png"
+            alt="">
+        <span class="Pokemon">
+            <h3 class="PokemonName">Pikachu</h3>
+            <div class="PokemonTypes">
+                <h4 class="EntryNumber">#17</h4>
+                <h4 class="Type1">Electric</h4>
+                <h4 class="Type2">Flying</h4>
+            </div>
+        </span>
+
+    </div>
+    <div class="ExtraContentHidden">
+        <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto adipisci dolore aliquam et, ut
+            porro
+            doloremque minima ipsam rerum vel?</h4>
+        <h5>First Appeared: Sinnoh</h5>
+    </div>
+</div>
+
+*/
