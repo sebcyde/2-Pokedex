590ab4098445e59eb69e01ea0fa906d7521cde2c
Seb <sebcyde@hotmail.com>
2021-10-20 17:44:15 +0100

    added in sort by Entry High to Low still need to add in other sort functionality

--- app.js
+++ app.js
@@ -3,201 +3,262 @@
 const PokeDexEntryNumbers = [];
 const PokemonNameList = [];
 
-function isInViewport(element) {
-	const rect = element.getBoundingClientRect();
-	return (
-		rect.top >= 0 &&
-		rect.left >= 0 &&
-		rect.bottom <=
-			(window.innerHeight || document.documentElement.clientHeight) &&
-		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-	);
-}
+document.querySelector('.ENL2H').selected = true;
 
 document.querySelector('#mbps').addEventListener('click', () => {
 	document.querySelector('.LeftSection').scrollTo(0, 0);
 	console.log('Clicked');
 });
+let time = 1;
 
-axios
-	.get('https://pokeapi.co/api/v2/pokemon?limit=1118')
-	.then((results) => {
-		let RawResults = results.data.results;
-		const LoadingSignal = document.querySelector('#LoadingSignal');
-		LoadingSignal.classList.add('Hidden');
+function Create(time) {
+	axios
+		.get('https://pokeapi.co/api/v2/pokemon?limit=1118')
+		.then((results) => {
+			if (time > 1) {
+				let PC = document.querySelector('div.PokeCardContainer');
+				let PCAll = document.querySelectorAll('div.PokeCardContainer');
+				PCAll.forEach((C) => {
+					C.classList.add('Hidden');
+				});
+				document.getElementById('LoadingSignal').classList.remove('Hidden');
 
-		RawResults.map((Poke) => {
-			const x = Poke.name;
-			const PokeCardContainer = document.createElement('div');
-
-			PokeCardContainer.classList.add('PokeCardContainer');
-			axios.get(`https://pokeapi.co/api/v2/pokemon/${x}`).then((results) => {
-				const RawPokeData = results.data;
-
-				const PokeCard = document.createElement('div');
-				PokeCard.classList.add('PokeCard');
-				PokeCardContainer.appendChild(PokeCard);
-				const PokemonSprite = document.createElement('img');
-				PokemonSprite.src = '/assets/LoadingSignal.gif';
-
-				PokemonSprite.src = RawPokeData.sprites.front_default;
-				PokeCard.appendChild(PokemonSprite);
-				const PokemonSpan = document.createElement('span');
-				PokemonSpan.classList.add('Pokemon');
-				PokeCardContainer.appendChild(PokemonSpan);
-				const PokemonName = document.createElement('h3');
-				PokemonName.classList.add('PokemonName');
-				const PName = Poke.name;
-				const CappedPname = PName.charAt(0).toUpperCase() + PName.slice(1);
-				PokemonName.innerText = CappedPname;
-				PokemonNameList.push(Poke.name);
-				PokemonSpan.appendChild(PokemonName);
-				const PokemonTypes = document.createElement('div');
-				PokemonTypes.classList.add('PokemonTypes');
-				PokemonSpan.appendChild(PokemonTypes);
-				const PokemonEntryNumber = document.createElement('h4');
-				PokemonEntryNumber.classList.add('EntryNumber');
-				PokemonEntryNumber.innerText = ` #${RawPokeData.id}`;
-				PokeDexEntryNumbers.push(RawPokeData.id);
-				PokemonTypes.appendChild(PokemonEntryNumber);
-				const PokemonType1 = document.createElement('h4');
-				PokemonType1.classList.add('Type1');
-				PokemonType1.innerText = RawPokeData.types[0].type.name;
-				PokemonType1.classList.add(RawPokeData.types[0].type.name);
-				PokemonTypes.appendChild(PokemonType1);
-				if (RawPokeData.types.length > 1) {
-					const PokemonType2 = document.createElement('h4');
-					PokemonType2.classList.add('Type2');
-					PokemonType2.innerText = RawPokeData.types[1].type.name;
-					PokemonType2.classList.add(RawPokeData.types[1].type.name);
-					PokemonTypes.appendChild(PokemonType2);
+				if (document.querySelector('.ENH2L').selected === true) {
+					if (
+						document.querySelector('section#LeftSection').contains(PC) === true
+					) {
+						PCAll.forEach((Card) => {
+							Card.parentNode.removeChild(Card);
+						});
+					}
+					console.log('Sorted by entry number H2L');
+					results.data.results.reverse();
+				} else if (document.querySelector('.ENL2H').selected === true) {
+					if (
+						document.querySelector('section#LeftSection').contains(PC) === true
+					) {
+						PCAll.forEach((Card) => {
+							Card.parentNode.removeChild(Card);
+						});
+					}
+					console.log('Sorted by entry number L2H');
+				} else if (document.querySelector('.AA2Z').selected === true) {
+					if (
+						document.querySelector('section#LeftSection').contains(PC) === true
+					) {
+						PCAll.forEach((Card) => {
+							Card.parentNode.removeChild(Card);
+						});
+					}
+					results.data.results.sort();
+				} else if (document.querySelector('.AZ2A').selected === true) {
+					if (
+						document.querySelector('section#LeftSection').contains(PC) === true
+					) {
+						PCAll.forEach((Card) => {
+							Card.parentNode.removeChild(Card);
+						});
+					}
+					results.data.results.sort();
+					results.data.results.reverse();
 				}
-				PokeCard.appendChild(PokemonSpan);
-				const EDDiv = document.createElement('div');
-				EDDiv.classList.add('SelectedMarker');
+			}
 
-				EDDiv.classList.add('Hidden');
-				const BaseXP = document.createElement('h5');
-				BaseXP.innerText = `Base XP: ${RawPokeData.base_experience}`;
-				EDDiv.appendChild(BaseXP);
-				const Height = document.createElement('h5');
-				Height.innerText = `Height: ${RawPokeData.height}`;
-				EDDiv.appendChild(Height);
-				const Weight = document.createElement('h5');
-				Weight.innerText = `Weight: ${RawPokeData.weight}`;
-				EDDiv.appendChild(Weight);
-				PokeCard.appendChild(EDDiv);
+			const LoadingSignal = document.querySelector('#LoadingSignal');
 
-				PokeCard.addEventListener('click', (event) => {
-					const AllPokeCards = document.querySelectorAll('div.PokeCard');
-					const AllEDDivs = document.querySelectorAll('div.SelectedMarker');
-					console.log(AllPokeCards);
+			console.log(results.data.results);
 
-					Array.from(AllPokeCards).map((PCard) => {
-						if (PCard.classList.contains('Selected')) {
-							PCard.classList.remove('Selected');
-							// Array.from(AllEDDivs).map((EDivs) => {
-							// 	EDivs.classList.add('Hidden');
-							// 	EDivs.classList.remove('EDDiv');
-							// });
-						}
-					});
+			results.data.results.map((Poke) => {
+				const x = Poke.name;
+				let PokeCardContainer = document.createElement('div');
+				PokeCardContainer.classList.add('PokeCardContainer');
+				PokeCardContainer.classList.add('Hidden');
 
-					if (!PokeCard.classList.contains('Selected')) {
-						PokeCard.classList.add('Selected');
-						// EDDiv.classList.remove('Hidden');
-						// EDDiv.classList.add('EDDiv');
+				// default PokeCard List
+				axios.get(`https://pokeapi.co/api/v2/pokemon/${x}`).then((results) => {
+					const RawPokeData = results.data;
 
+					const PokeCard = document.createElement('div');
+					PokeCard.classList.add('PokeCard');
+					PokeCardContainer.appendChild(PokeCard);
+					const PokemonSprite = document.createElement('img');
+					PokemonSprite.src = 'assets/LoadingSignal.gif';
+
+					PokemonSprite.src = RawPokeData.sprites.front_default;
+					PokeCard.appendChild(PokemonSprite);
+					const PokemonSpan = document.createElement('span');
+					PokemonSpan.classList.add('Pokemon');
+					PokeCardContainer.appendChild(PokemonSpan);
+					const PokemonName = document.createElement('h3');
+					PokemonName.classList.add('PokemonName');
+					const PName = Poke.name;
+					const CappedPname = PName.charAt(0).toUpperCase() + PName.slice(1);
+					PokemonName.innerText = CappedPname;
+					PokemonNameList.push(Poke.name);
+					PokemonSpan.appendChild(PokemonName);
+					const PokemonTypes = document.createElement('div');
+					PokemonTypes.classList.add('PokemonTypes');
+					PokemonSpan.appendChild(PokemonTypes);
+					const PokemonEntryNumber = document.createElement('h4');
+					PokemonEntryNumber.classList.add('EntryNumber');
+					PokemonEntryNumber.innerText = ` #${RawPokeData.id}`;
+					PokeDexEntryNumbers.push(RawPokeData.id);
+					PokemonTypes.appendChild(PokemonEntryNumber);
+					const PokemonType1 = document.createElement('h4');
+					PokemonType1.classList.add('Type1');
+					PokemonType1.innerText = RawPokeData.types[0].type.name;
+					PokemonType1.classList.add(RawPokeData.types[0].type.name);
+					PokemonTypes.appendChild(PokemonType1);
+					if (RawPokeData.types.length > 1) {
+						const PokemonType2 = document.createElement('h4');
+						PokemonType2.classList.add('Type2');
+						PokemonType2.innerText = RawPokeData.types[1].type.name;
+						PokemonType2.classList.add(RawPokeData.types[1].type.name);
+						PokemonTypes.appendChild(PokemonType2);
+					}
+					PokeCard.appendChild(PokemonSpan);
+					const EDDiv = document.createElement('div');
+					EDDiv.classList.add('SelectedMarker');
+
+					EDDiv.classList.add('Hidden');
+					const BaseXP = document.createElement('h5');
+					BaseXP.innerText = `Base XP: ${RawPokeData.base_experience}`;
+					EDDiv.appendChild(BaseXP);
+					const Height = document.createElement('h5');
+					Height.innerText = `Height: ${RawPokeData.height}`;
+					EDDiv.appendChild(Height);
+					const Weight = document.createElement('h5');
+					Weight.innerText = `Weight: ${RawPokeData.weight}`;
+					EDDiv.appendChild(Weight);
+					PokeCard.appendChild(EDDiv);
+
+					PokeCard.addEventListener('click', (event) => {
 						const AllPokeCards = document.querySelectorAll('div.PokeCard');
+						const AllEDDivs = document.querySelectorAll('div.SelectedMarker');
 						console.log(AllPokeCards);
 
 						Array.from(AllPokeCards).map((PCard) => {
 							if (PCard.classList.contains('Selected')) {
-								let x =
-									PCard.childNodes[1].childNodes[0].innerText.toLowerCase();
-								document.querySelector('.RightPokemonName').innerText =
-									PCard.childNodes[1].childNodes[0].innerText;
-
-								axios
-									.get(`https://pokeapi.co/api/v2/pokemon/${x}`)
-									.then((results) => {
-										document.querySelector('.RightTypes').innerHTML = '';
-
-										console.log(results.data);
-										const Info = results.data;
-
-										document.querySelector(
-											'.RightEntryNumber'
-										).innerText = `Entry Number #${Info.id}`;
-
-										document.querySelector('.FrontImage').src =
-											Info.sprites.front_default;
-
-										document.querySelector('.BackImage').src =
-											Info.sprites.back_default;
-										document.querySelector('.ShinyFront').src =
-											Info.sprites.front_shiny;
-										document.querySelector('.ShinyBack').src =
-											Info.sprites.back_shiny;
-
-										const RT1 = document.createElement('div');
-										RT1.classList.add(Info.types[0].type.name);
-										document.querySelector('.RightTypes').appendChild(RT1);
-
-										RT1.innerText = Info.types[0].type.name;
-
-										if (Info.types.length > 1) {
-											const RT2 = document.createElement('div');
-											RT2.classList.add(Info.types[1].type.name);
-											document.querySelector('.RightTypes').appendChild(RT2);
-											RT2.innerText = Info.types[1].type.name;
-										}
-
-										document.querySelector(
-											'.BXP'
-										).innerText = `Base XP: ${Info.base_experience}`;
-										document.querySelector(
-											'.H'
-										).innerText = `Height: ${Info.height}`;
-										document.querySelector(
-											'.W'
-										).innerText = `Weight: ${Info.weight}`;
-
-										document.querySelector('.RightRightStats').innerHTML = '';
-
-										const h2 = document.createElement('h2');
-										h2.classList.add('AbilitiesHeader');
-										h2.innerText = 'Abilities';
-										document.querySelector('.RightRightStats').appendChild(h2);
-
-										for (
-											let index = 0;
-											index < Info.abilities.length;
-											index++
-										) {
-											const Ab = document.createElement('p');
-											Ab.classList.add('Ability');
-											Ab.innerText = Info.abilities[index].ability.name;
-											document
-												.querySelector('.RightRightStats')
-												.appendChild(Ab);
-										}
-									})
-									.catch((err) => {
-										console.log(err);
-									});
+								PCard.classList.remove('Selected');
+								// Array.from(AllEDDivs).map((EDivs) => {
+								// 	EDivs.classList.add('Hidden');
+								// 	EDivs.classList.remove('EDDiv');
+								// });
 							}
 						});
-					}
-				});
-			});
-			document.querySelector('.LeftSection').appendChild(PokeCardContainer);
-		});
 
-		//Sort PokeDex Logic
-		console.log(PokemonNameList);
-		console.log(PokeDexEntryNumbers);
-	})
-	.catch((error) => {
-		console.log(error);
-	});
+						if (!PokeCard.classList.contains('Selected')) {
+							PokeCard.classList.add('Selected');
+							// EDDiv.classList.remove('Hidden');
+							// EDDiv.classList.add('EDDiv');
+
+							const AllPokeCards = document.querySelectorAll('div.PokeCard');
+							console.log(AllPokeCards);
+
+							Array.from(AllPokeCards).map((PCard) => {
+								if (PCard.classList.contains('Selected')) {
+									let x =
+										PCard.childNodes[1].childNodes[0].innerText.toLowerCase();
+									document.querySelector('.RightPokemonName').innerText =
+										PCard.childNodes[1].childNodes[0].innerText;
+
+									axios
+										.get(`https://pokeapi.co/api/v2/pokemon/${x}`)
+										.then((results) => {
+											document.querySelector('.RightTypes').innerHTML = '';
+
+											console.log(results.data);
+											const Info = results.data;
+
+											document.querySelector(
+												'.RightEntryNumber'
+											).innerText = `Entry Number #${Info.id}`;
+
+											document.querySelector('.FrontImage').src =
+												Info.sprites.front_default;
+
+											document.querySelector('.BackImage').src =
+												Info.sprites.back_default;
+											document.querySelector('.ShinyFront').src =
+												Info.sprites.front_shiny;
+											document.querySelector('.ShinyBack').src =
+												Info.sprites.back_shiny;
+
+											const RT1 = document.createElement('div');
+											RT1.classList.add(Info.types[0].type.name);
+											document.querySelector('.RightTypes').appendChild(RT1);
+
+											RT1.innerText = Info.types[0].type.name;
+
+											if (Info.types.length > 1) {
+												const RT2 = document.createElement('div');
+												RT2.classList.add(Info.types[1].type.name);
+												document.querySelector('.RightTypes').appendChild(RT2);
+												RT2.innerText = Info.types[1].type.name;
+											}
+
+											document.querySelector(
+												'.BXP'
+											).innerText = `Base XP: ${Info.base_experience}`;
+											document.querySelector(
+												'.H'
+											).innerText = `Height: ${Info.height}`;
+											document.querySelector(
+												'.W'
+											).innerText = `Weight: ${Info.weight}`;
+
+											document.querySelector('.RightRightStats').innerHTML = '';
+
+											const h2 = document.createElement('h2');
+											h2.classList.add('AbilitiesHeader');
+											h2.innerText = 'Abilities';
+											document
+												.querySelector('.RightRightStats')
+												.appendChild(h2);
+
+											for (
+												let index = 0;
+												index < Info.abilities.length;
+												index++
+											) {
+												const Ab = document.createElement('p');
+												Ab.classList.add('Ability');
+												Ab.innerText = Info.abilities[index].ability.name;
+												document
+													.querySelector('.RightRightStats')
+													.appendChild(Ab);
+											}
+										})
+										.catch((err) => {
+											console.log(err);
+										});
+								}
+							});
+						}
+					});
+				});
+				document.querySelector('.LeftSection').appendChild(PokeCardContainer);
+
+				PokeCardContainer.classList.remove('Hidden');
+				LoadingSignal.classList.add('Hidden');
+			});
+
+			console.log(time);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
+
+//Sort PokeDex Logic
+
+Create(time);
+const SortOptions = document.querySelectorAll('option');
+console.log(SortOptions);
+
+document.querySelector('#MobilePokeSorter').addEventListener('change', () => {
+	time++;
+	console.log(time);
+	console.log(document.querySelector('#MobilePokeSorter').value);
+	Create(time);
+});
